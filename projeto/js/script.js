f1fdcb62c93649804e7f4f9a889499ebf60a05a5
HudsonCarneiro <161895030+HudsonCarneiro@users.noreply.github.com>
2024-08-08 11:36:30 -0300

    tela adicionada

--- projeto/js/script.js
+++ projeto/js/script.js
@@ -5,3 +5,31 @@ function abrirNovaSenha() {
 function abrirIndex(){
     window.location.href = "index.html";
 }
+function abrirHome(){
+    window.location.href = "home.html";
+}
+function abrirCadastrarMotorista(){
+    window.location.href = "cadastrar-motorista.html";
+}
+
+let nomesMotoristas = [];
+
+function addMotorista(){
+    const nomeMotorista = document.getElementById("name-driver").value;
+    if(nomeMotorista.trim() !==""){
+        nomesMotoristas.push(nomeMotorista);
+        atualizarLista();
+    }
+}
+
+function atualizarLista(){
+    const lista = document.getElementById("listaMotoristas");
+
+    lista.innerHTML = "";
+
+    nomesMotoristas.forEach(function(nomeMotorista){
+        const li = document.createElement("li");
+        li.innerText = nomeMotorista;
+        lista.appendChild(li)
+    })
+}
