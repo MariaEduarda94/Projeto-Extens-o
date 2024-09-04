1e544ac4541a9c4dc5712d4ca277a3e186116dd9
HudsonCarneiro <161895030+HudsonCarneiro@users.noreply.github.com>
2024-09-04 09:06:25 -0300

    Revert "login style"
    This reverts commit ac628c6e096a9b08ac789c65464b14ca4a5c4c41.

---
+++ projeto/js/carro.js
@@ -0,0 +1,8 @@
+class Carro{
+    constructor(nome, montadora, placa, anoFabricacao){
+        this.nome = nome;
+        this.montadora = montadora;
+        this.placa = placa;
+        this.anoFabricacao = anoFabricacao;
+    }
+}
--- projeto/js/script.js
+++
@@ -1,7 +0,0 @@
-function abrirNovaSenha() {
-    window.location.href ="nova-senha.html";
-}
-
-function abrirIndex(){
-    window.location.href = "index.html";
-}
