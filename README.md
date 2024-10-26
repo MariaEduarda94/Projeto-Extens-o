2300b62b9f1d754ab05205317696ad5391e24dcd
HudsonCarneiro <161895030+HudsonCarneiro@users.noreply.github.com>
2024-10-26 17:09:48 -0300

    delete

--- README.md
+++
@@ -1,2 +0,0 @@
-# Projeto de Extensão
-Projeto de extensão focado no gerenciamento de frota.
--- projeto/js/usuario.js
+++
@@ -1,8 +0,0 @@
-class Usuario{
-    constructor(nome, cpf, email, dataNascimento){
-        this.nome = nome;
-        this.cpf = cpf;
-        this.email = email;
-        this.dataNascimento = dataNascimento;
-    }
-}
